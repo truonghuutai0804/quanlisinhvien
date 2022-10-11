a1bee30c75bb1a05ab033af3da860b4847e4adb8
truonghuutai <truonghuutai@gmail.com>
2022-10-12 00:47:34 +0700

    12/10/2022 lần 1

--- src/app/controllers/FacultyController.js
+++ src/app/controllers/FacultyController.js
@@ -3,12 +3,57 @@ const { QueryTypes } = require('sequelize');
 
 class FacultyController {
     // [GET] /api/faculty
-    async index(req,res){
+    async index(req,res, next){
         try {
-            const data = await sequelize.query(`SELECT * FROM faculties`, { type: QueryTypes.SELECT, })
+            const data = await sequelize.query(`SELECT * FROM faculties`, { type: QueryTypes.SELECT })
             return res.json({
                 data: data,
                 status: 400
+            })            
+        } catch (error) {
+            console.log('Lỗi nhá:', error)
+        }
+    }
+
+    // [POST] /api/faculty
+    async create(req,res, next){
+        try {
+            const maKhoa = req.body.MA_KHOA
+            const tenKhoa = req.body.TEN_KHOA
+            await sequelize.query(`INSERT INTO faculties (MA_KHOA, TEN_KHOA)`+
+                                        ` VALUES ('${maKhoa}', '${tenKhoa}')`, { type: QueryTypes.INSERT })
+            return res.json({
+                message: 'SUCCESS'
+            })            
+        } catch (error) {
+            console.log('Lỗi nhá:', error)
+        }
+    }
+
+    // [PUT] /api/faculty/:id
+    async update(req,res, next){
+        try {
+            const maKhoa = req.params.id
+            const tenKhoa = req.body.TEN_KHOA
+            await sequelize.query(`UPDATE faculties
+                                    SET TEN_KHOA = '${tenKhoa}'
+                                    WHERE MA_KHOA LIKE '%${maKhoa}';`, { type: QueryTypes.UPDATE })
+            return res.json({
+                message: 'SUCCESS'
+            })            
+        } catch (error) {
+            console.log('Lỗi nhá:', error)
+        }
+    }
+
+    // [DELETE] /api/faculty/:id
+    async delete(req,res, next){
+        try {
+            const maKhoa = req.body.MA_KHOA
+            await sequelize.query(`DELETE FROM faculties 
+                                    WHERE MA_KHOA LIKE'${maKhoa}'`, { type: QueryTypes.DELETE })
+            return res.json({
+                message: 'SUCCESS'
             })            
         } catch (error) {
             console.log('Lỗi nhá:', error)
--- src/routes/faculty.js
+++ src/routes/faculty.js
@@ -5,4 +5,11 @@ const facultyController = require('../app/controllers/FacultyController')
 
 router.get('/api/faculty', facultyController.index)
 
+router.post('/api/faculty', facultyController.create)
+
+router.put('/api/faculty/:id', facultyController.update)
+
+router.delete('/api/faculty/:id', facultyController.delete)
+
+
 module.exports = router
